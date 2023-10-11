869cfb80987a4bef5ddb29e8286c317b2cac15ba
JulieMaeVivas <144203772+JulieMaeVivas@users.noreply.github.com>
2023-10-11 12:51:36 +0800

    Add files via upload

---
+++ JULIEACTIVITY4.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package julie.activity;
+
+/**
+ *
+ * @author Vivas_CPE
+ */
+public class JULIEACTIVITY {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10000; i++) {
+            if (i % 2 != 0) {
+                System.out.println(i); // Print the odd number
+            }
+        }
+        
+    
+ 
